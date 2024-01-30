6e2603bb634e737c258b6e6b35fe7b2e22234f8e
learnuidev <learnuidev@gmail.com>
2024-01-30 15:48:16 -0500

    Added config validation

--- config/app-config.ts
+++ config/app-config.ts
@@ -1,3 +1,11 @@
+import { z } from "zod";
+
+const configSchema = z.object({
+  openAIAPIKey: z.string(),
+  pineconeAPIKey: z.string(),
+  youtubeAPIKey: z.string(),
+});
+
 export interface IAppConfig {
   openAIAPIKey: string;
   pineconeAPIKey: string;
@@ -7,5 +15,7 @@ export interface IAppConfig {
 export const appConfig = {
   youtubeAPIKey: process.env.YOUTUBE_API_KEY,
   openAIAPIKey: process.env.OPENAI_API_KEY,
-  pineconeAPIKey: process.env.PINECONE_API_KEY,
+  // pineconeAPIKey: process.env.PINECONE_API_KEY,
 } as IAppConfig;
+
+configSchema.parse(appConfig);
