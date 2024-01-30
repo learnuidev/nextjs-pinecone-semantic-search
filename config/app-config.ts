184c8eab13737e121a0136c710750867157aec09
learnuidev <learnuidev@gmail.com>
2024-01-30 15:50:16 -0500

    make it pass

--- config/app-config.ts
+++ config/app-config.ts
@@ -15,7 +15,7 @@ export interface IAppConfig {
 export const appConfig = {
   youtubeAPIKey: process.env.YOUTUBE_API_KEY,
   openAIAPIKey: process.env.OPENAI_API_KEY,
-  // pineconeAPIKey: process.env.PINECONE_API_KEY,
+  pineconeAPIKey: process.env.PINECONE_API_KEY,
 } as IAppConfig;
 
 configSchema.parse(appConfig);
